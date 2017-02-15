da9ae745bdf2f3cf5177bcbb04bfee25a5535df1
Sujata Dev <sujatadev97@gmail.com>
2017-02-16 01:36:02 +0530

    inputting polynomial values and creating it using linked list

---
+++ src/Polynomial addition using linkedlist.c
@@ -0,0 +1,78 @@
+//Polynomial addition using linked list
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    float coef;
+    int exp;
+    struct node *link;
+};
+struct node *input(struct node *start);
+struct node *create(struct node *start,float co,int ex);
+void display(struct node *ptr);
+struct node *add(struct node *p1,struct node *p2);
+int main()
+{
+    struct node *start1=NULL,*start2=NULL,*start3=NULL;
+    printf("Polynomial1:\n");
+    start1=input(start1);
+    printf("Polynomial2:\n");
+    start2=input(start2);
+    printf("Polynomial1 is:");
+    display(start1);
+    printf("Polynomial2 is:");
+    display(start2);
+}
+struct node *input(struct node *start)
+{
+    int i,n,ex;
+    float co;
+    printf("No of terms to be entered: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("Enter coefficient for term %d: ",i);
+        scanf("%f",&co);
+        printf("Enter exponent for term %d: ",i);
+        scanf("%d",&ex);
+        start=create(start,co,ex);
+    }
+    return start;
+}
+struct node *create(struct node *start,float co,int ex)
+{
+    struct node *ptr,*tmp;
+    tmp=malloc(sizeof(struct node));
+    tmp->coef=co;
+    tmp->exp=ex;
+    if(start==NULL ||ex>start->exp)
+    {
+        tmp->link=start;
+        start=tmp;
+    }
+    else
+    {
+        ptr=start;
+        while(ptr->link!=NULL && ptr->link->exp>ex)
+            ptr=ptr->link;
+        tmp->link=ptr->link;
+        ptr->link=tmp;
+        if(ptr->link==NULL)
+            tmp->link=NULL;
+    }
+    return start;
+}
+void display(struct node *ptr)
+{
+    if(ptr==NULL)
+    {
+        printf("Empty\n");
+        return;
+    }
+    while(ptr!=NULL)
+    {
+        printf(" (%.1fx^%d) +",ptr->coef,ptr->exp);
+        ptr=ptr->link;
+    }
+    printf("\b \b\n\n");
+}
